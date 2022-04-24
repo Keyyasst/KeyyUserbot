f2ad0eb493136773aa54bd3cedd00881222aa332
shiroxynn <94505216+ReyyNada@users.noreply.github.com>
2022-04-25 03:19:04 +0700

    Update toxic.py

--- userbot/modules/toxic.py
+++ userbot/modules/toxic.py
@@ -2,125 +2,125 @@
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_or_reply, bdrl_cmd
+from userbot.utils import edit_or_reply, keyy_cmd
 
 
-@bdrl_cmd(pattern="^A(?: |$)(.*)")
+@keyy_cmd(pattern="^A(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**ANJING LU YA KETEMU GUA TAMPOL LUUU**")
 
 
-@bdrl_cmd(pattern="^B(?: |$)(.*)")
+@keyy_cmd(pattern="^B(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**BANYAK TINGKAH LU, BIAR APA SI BEGITU,NAJIS CUIHHH**")
 
 
-@bdrl_cmd(pattern="^C(?: |$)(.*)")
+@keyy_cmd(pattern="^C(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**CIAHHHH ELAH ,MUKA KEK BABI AJA SO SO AN NYARI CEWE GUD LUKING**")
 
 
-@bdrl_cmd(pattern="^D(?: |$)(.*)")
+@keyy_cmd(pattern="^D(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**DASAR ANAK HINA LU ,HASIL KAWIN SILANG SETAN SAMA HANSIP KOMPLEK**")
 
 
-@bdrl_cmd(pattern="^E(?: |$)(.*)")
+@keyy_cmd(pattern="^E(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**EMANG YANG KEK GINI NIH HARUS DIBERI PAHAM ,ANJING LO !!**")
 
 
-@bdrl_cmd(pattern="^F(?: |$)(.*)")
+@keyy_cmd(pattern="^F(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**BABI!!KONTOL!!NGENTOT!!!**")
 
 
-@bdrl_cmd(pattern="^G(?: |$)(.*)")
+@keyy_cmd(pattern="^G(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GANTENGAN JUGA GUA GOBLOK, NGACA NAPAH MUKA LU DAH KEK BABI AER**")
 
 
-@bdrl_cmd(pattern="^H(?: |$)(.*)")
+@keyy_cmd(pattern="^H(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**HAHAHAHA OH INI YANG SOK JAGOAN DI TELE, BIAR APA SIH BEGITU, DASAR NOLEP GA PUNYA TEMEN ANJING, DIRUMAH JADI CUPU DITELE SOK MENINGGI KONTOL**")
 
 
-@bdrl_cmd(pattern="^I(?: |$)(.*)")
+@keyy_cmd(pattern="^I(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
 
 
-@bdrl_cmd(pattern="^J(?: |$)(.*)")
+@keyy_cmd(pattern="^J(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**JANCOKKKK ,NGENTOOOTTTTTTTT ,BANGSATTTTTTT ,MEMEKKKKKKKKK!!**")
 
 
-@bdrl_cmd(pattern="^K(?: |$)(.*)")
+@keyy_cmd(pattern="^K(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**KONTOL MASIH ITEM AJA SO SO AN NGAJAK VCS ANJING?**")
 
 
-@bdrl_cmd(pattern="^M(?: |$)(.*)")
+@keyy_cmd(pattern="^M(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MEMEK LU INTEM ANJENGGG BAU TERASI IDIHHHH!!**")
 
 
-@bdrl_cmd(pattern="^N(?: |$)(.*)")
+@keyy_cmd(pattern="^N(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**NAJISSSS INI GRUP APA KUBURAN SEPI BET NGENTOTTT ,BUBARIN AJALAH ANJING!!**")
 
 
-@bdrl_cmd(pattern="^O(?: |$)(.*)")
+@keyy_cmd(pattern="^O(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**ORANG ANJING LUH ,KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
 
 
-@bdrl_cmd(pattern="^Q(?: |$)(.*)")
+@keyy_cmd(pattern="^Q(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GAUSAH SOKAP DEH KAMU, EMANG LU SIAPA ANJIRRR!!**")
 
 
-@bdrl_cmd(pattern="^R(?: |$)(.*)")
+@keyy_cmd(pattern="^R(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MAAF BUKAN JAGOAN HAHAHAH, GA KEK LU YANG SO JAGOAN , MENTAL SOSMED CUIHHHH!!**")
 
 
-@bdrl_cmd(pattern="^S(?: |$)(.*)")
+@keyy_cmd(pattern="^S(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**SANGE KOK VIRTUAL ANJING, KECIRI BET ORANG MISKIN KONTOL ,KALO MAU ENAK YA BAYAR BABI!!**")
 
 
-@bdrl_cmd(pattern="^T(?: |$)(.*)")
+@keyy_cmd(pattern="^T(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**TETEK LU TUH TEPOS GOBLOK, JANGAN SOK SOK AN TIPIS TIPIS ,IH NAJIS CUIHHH**")
 
 
-@bdrl_cmd(pattern="^U(?: |$)(.*)")
+@keyy_cmd(pattern="^U(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GANTENG LU BEGITU HAHHHHH???**")
 
 
-@bdrl_cmd(pattern="^V(?: |$)(.*)")
+@keyy_cmd(pattern="^V(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**CANTIK LU BEGITU, MUKA MSH PAKE FILTER IG JAN SO KERAS**")
 
 
-@bdrl_cmd(pattern="^W(?: |$)(.*)")
+@keyy_cmd(pattern="^W(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**War War Tai Anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**")
 
     
-@bdrl_cmd(pattern="^X(?: |$)(.*)")
+@keyy_cmd(pattern="^X(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MENTANG MENTANG PUNYA BOT MAINNYA BOT MULU!!NORAK BET SI ANJINGGGG HAHAHAHH**")
     
     
-@bdrl_cmd(pattern="^Y(?: |$)(.*)")
+@keyy_cmd(pattern="^Y(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**BUAT LO KONTOL NIH KALO UDAH HINA GAUSAH SOK SOK NGEHINA HINA GUA KONTOL, GUA TERLALU SUCI BUAT LU YANG HINA ITU ADUHHH. SINI GUA LUDAHIN DLU LU BIAR DIRI LU SUCI KARENA LU TAU LUDAH GUA ITU MULIA SEKALI**",)
     
     
-@bdrl_cmd(pattern="^Z(?: |$)(.*)")
+@keyy_cmd(pattern="^Z(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**EH TOLOL LU LAGI NGATAIN DIRI SENDIRI YEH WKWKWK KASIAN BATT KASIAN BARU LIAT MUKA LU AJA UDAH KETEBAK KALAU LU TUH JABLAY TELE YANG HOBBY NYA TUH SUKA OMEK² DEPAN UMUM WKWK YANG KALAU DI BAYAR PAKE DUIT RECEHAN JUGA MASIH TTP DI GAS YAHHH KETAHUAN KAN MAKANYA KALAU MAU NGATAIN ORANG TUH NGACA DLU BHAAKKS EHH LUPA LU KAN KGK PUNYA KACA TOLOL ORANG MISKIN YANG BERLAGAK ORANG KAYA MANA PUNYA KACA WKWK**")  
     
