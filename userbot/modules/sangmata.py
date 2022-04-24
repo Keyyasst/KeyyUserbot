041467d91c090331f7e5d7ebaa36a84c6fee927c
shiroxynn <94505216+ReyyNada@users.noreply.github.com>
2022-04-25 03:33:37 +0700

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -15,11 +15,11 @@ from userbot.utils import (
     edit_delete,
     edit_or_reply,
     get_user_from_event,
-    bdrl_cmd,
+    keyy_cmd,
 )
 
 
-@bdrl_cmd(pattern="sg(u)?(?:\s|$)([\s\S]*)")
+@keyy_cmd(pattern="sg(u)?(?:\s|$)([\s\S]*)")
 async def _(event):
     input_str = "".join(event.text.split(maxsplit=1)[1:])
     reply_message = await event.get_reply_message()
