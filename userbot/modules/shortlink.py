f9a4a48b2809d01c0fe6bf8103b90bcb2000d087
shiroxynn <94505216+ReyyNada@users.noreply.github.com>
2022-04-25 03:29:58 +0700

    Update shortlink.py

--- userbot/modules/shortlink.py
+++ userbot/modules/shortlink.py
@@ -10,10 +10,10 @@ from telethon.tl.functions.contacts import UnblockRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_or_reply, bdrl_cmd
+from userbot.utils import edit_or_reply, keyy_cmd
 
 
-@bdrl_cmd(pattern="short(?: |$)(.*)")
+@keyy_cmd(pattern="short(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
