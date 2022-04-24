71b824007b4bed819be6e15f74913199ca89dc1a
Keyyasst <101421795+Keyyasst@users.noreply.github.com>
2022-04-25 04:26:30 +0700

    Update fakegban.py

--- userbot/modules/fakegban.py
+++ userbot/modules/fakegban.py
@@ -7,10 +7,10 @@ from telethon.tl.types import ChannelParticipantsAdmins
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, bot, owner
-from userbot.utils import edit_or_reply, bdrl_cmd
+from userbot.utils import edit_or_reply, keyy_cmd
 
 
-@bdrl_cmd(pattern="fgban(?: |$)(.*)")
+@keyy_cmd(pattern="fgban(?: |$)(.*)")
 async def gbun(event):
     if event.fwd_from:
         return
