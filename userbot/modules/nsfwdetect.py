a7bf500546ee7e316a6ad6b6e38a39e598090b53
Brian <101421795+Keyyasst@users.noreply.github.com>
2022-04-25 09:06:45 +0700

    Update nsfwdetect.py

--- userbot/modules/nsfwdetect.py
+++ userbot/modules/nsfwdetect.py
@@ -8,11 +8,11 @@ import os
 import requests
 
 from userbot import CMD_HELP, DEEP_AI, bot
-from userbot.events import bdrl_cmd
+from userbot.events import keyy_cmd
 from userbot.utils import edit_delete, edit_or_reply
 
 
-@bot.on(bdrl_cmd(outgoing=True, pattern=r"detect$"))
+@bot.on(keyy_cmd(outgoing=True, pattern=r"detect$"))
 async def detect(event):
     if DEEP_AI is None:
         return await edit_delete(
